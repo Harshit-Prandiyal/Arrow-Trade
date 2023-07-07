4e6efa9bf085a47bc28c45cfb26223b4a6030902
Harshit-Prandiyal <harshitprandiyal@gmail.com>
2023-07-07 15:59:01 +0530

    initialising app

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import {useFonts} from 'expo-font';
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Eudoxus-Sans-Bold': require('./assets/fonts/EudoxusSans-Bold.ttf'),
+  });
+  if(!fontsLoaded){
+    return <View>
+      <Text>Loading...</Text>
+    </View>
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +24,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title:{
+    fontFamily:'Eudoxus-Sans-Bold',
   },
 });
---
+++ constants/colors.js
@@ -0,0 +1,7 @@
+export const Colors={
+    headingBlackBlue:'#03314B',
+    lightgray :'#8198A5',
+    pink:'#F61C7A',
+    Darkblue:'#3500d4',
+    lightblue:'#908FEC',
+}
